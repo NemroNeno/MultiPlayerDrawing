1f8f273f24a95f9b4952f191f75fe24a103aaa54
nemro <mnabeel.bese22seecs.seecs.edu.pk>
2024-02-07 17:53:00 +0500

    First Official Commit from Pc-Local

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect, useRef, useLayoutEffect } from "react";
+import Fyro from "./Fyro";
+import SubscribeCard from "./SubscribeCard";
+import CreateRoom from "./components/CreateRoom";
+import { Route, Routes } from "react-router-dom";
+import JoinRoom from "./components/JoinRoom";
+import io from "socket.io-client";
+const server = "http://localhost:5000";
 
 function App() {
+  const socket = io.connect(server);
+  const [darkMode, setDarkMode] = useState(false);
+  const [user, setUser] = useState("");
+  const [roomId, setRoomId] = useState(null);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Routes>
+        <Route
+          path="/"
+          element={<CreateRoom socket={socket} setUser={setUser} setRoomIdProp={setRoomId} />}
+        />
+        <Route
+          path="/:roomId"
+          element={<JoinRoom socket={socket} user={user} roomId={roomId} />}
+        />
+      </Routes>
     </div>
   );
 }
